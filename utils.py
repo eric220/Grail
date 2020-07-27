581c4cef4550cd618512ddd624c7694945a6edba
Eric Criteser <ericcriteser@Erics-MBP.attlocal.net>
2020-07-26 22:16:59 -0500

    return continuous_cols, cleanup data_prep, add model_weights

--- utils.py
+++ utils.py
@@ -13,25 +13,28 @@ def get_permutations(df, num):
                 t_list.append(np.random.choice(df['{}' .format(c)].unique()))
             elif c == 'Input16':
                 t_list.append(np.random.randint(0,2))
+            elif c == 'Input14':
+                t_list.append(np.random.choice(df['{}' .format(c)].unique()))
             else:
                 t_list.append(random.random())
         t_lists.append(t_list)
         
     permutations_df = pd.DataFrame(t_lists)
     permutations_df.columns = df.columns.values
-    permutations_df.drop(['Target', 'log_Target'], axis = 1, inplace = True)
+    permutations_df.drop(['Target'], axis = 1, inplace = True)
     permutations_df = pd.get_dummies(permutations_df, columns=['Input1', 'Input2', 'Input3'])
     return permutations_df
 
-def xform_data(t_df, log = True, standard_scale = False):
+#gets log_Target, dummy 1-3, 
+def xform_data(t_df, standard_scale = False):
     if standard_scale == True:
         scaler = StandardScaler()
     else:
         scaler = MinMaxScaler()
     t_df['log_Target'] = np.log(t_df.Target)
+    t_df.drop('Target', axis = 1, inplace = True)
     df_dummied = pd.get_dummies(t_df, columns=['Input1', 'Input2', 'Input3'])
     continuous_cols = ['Input{}'.format(i) for i in range(4,16)]
-    continuous_cols.append('Target')
     continuous_cols.append('log_Target')
     df_dummied[continuous_cols] = scaler.fit_transform(df_dummied[continuous_cols])
-    return df_dummied, scaler
+    return df_dummied, scaler, continuous_cols
